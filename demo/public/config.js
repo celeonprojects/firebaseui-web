907d56ce0f9dfdd397682cebc01760231cd0e3f9
Lance Vo <lvo@vinavo.us>
2020-12-31 21:20:02 -0600

    Create config.js

---
+++ demo/public/config.js
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var config = {
+  apiKey: "AIzaSyD4NB9bVlGhEAqegZB4mdTusaM3NS-1kTQ",
+    authDomain: "check-in-51e9a.firebaseapp.com",
+    projectId: "check-in-51e9a",
+    storageBucket: "check-in-51e9a.appspot.com",
+    messagingSenderId: "305447058353",
+    appId: "1:305447058353:web:06a2ec9aade57d8fe61b90",
+    measurementId: "G-P673PGQE78"
+};
+firebase.initializeApp(config);
+
+var CLIENT_ID =
+    '265939374336-cj26lol6s990fq9fp4s0ri86si3rj86n.apps.googleusercontent.com'; // What is it?
